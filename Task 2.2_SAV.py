9d00bf668180dbba94eb1db4c992a0e79489b02b
A8820 <71602858+A8820@users.noreply.github.com>
2020-09-24 23:55:47 +0300

    Update Task 2.2_SAV.py

--- Task 2.2_SAV.py
+++ Task 2.2_SAV.py
@@ -1,13 +1,16 @@
 
 data_count = int(input("Введите количество элементов списка: "))
 data_box = []
+
 i = 0
 el = 0
+
 while i < data_count:
-    data_box.append(input("Введите следующее значение списка: "))
-    i += 1
+          data_box.append(input("Введите следующее значение списка: "))
+          i += 1
 
 for element in range(int(len(data_box)/2)):
-        data_box[el], data_box[el + 1] = data_box[el + 1], data_box[el]
-        el += 2
+          data_box[el], data_box[el + 1] = data_box[el + 1], data_box[el]
+          el += 2
+
 print(data_box)
