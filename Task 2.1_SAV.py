6cda8ff0844c8b21d06fc1b1ed13a2442861834f
A8820 <71602858+A8820@users.noreply.github.com>
2020-09-24 23:53:13 +0300

    Update Task 2.1_SAV.py

--- Task 2.1_SAV.py
+++ Task 2.1_SAV.py
@@ -2,7 +2,9 @@
 data = [2020, 'September', 22, 23.05, True]
 
 def data_type(el):
+    
     for el in range(len(data)):
-        print(type(data[el]))
+              print(type(data[el]))
     return
+
 data_type(data)
