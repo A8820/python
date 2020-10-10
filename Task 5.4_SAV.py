087111cdbb36de5e8f3c7522074730d4b41f0640
A8820 <71602858+A8820@users.noreply.github.com>
2020-10-10 21:26:45 +0300

    Delete Task 5.4_SAV.py

--- Task 5.4_SAV.py
+++
@@ -1,9 +0,0 @@
-rus_equal = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
-new_list_equal = []
-with open('Task 5.4_SAV.txt', 'r') as file_obj:
-    content = file_obj.read().split('\n')
-    for i in file_obj:
-        i = i.split(' ', 1)
-        new_list_equal.append(rus_equal[i[0]] + '  ' + i[1])
-    print(new_list_equal)
-
