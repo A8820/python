18a11f37df6b068e652ad843eaee21f3d3b44ebe
A8820 <71602858+A8820@users.noreply.github.com>
2020-10-10 18:30:58 +0300

    Delete Task 1.6_SAV.py

--- Task 1.6_SAV.py
+++
@@ -1,8 +0,0 @@
-
-a = int(input("Введите пробег первого дня в км: "))
-b = int(input("Введите цель в км: "))
-start_day_result = 1
-while a < b:
-        a *= 1.1
-        start_day_result += 1
-print(f"Вы достигнете требуемых показателей на %.d день" % start_day_result)
